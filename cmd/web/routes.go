d227faa023887ff20b6210ba914047721738a2f9
Victor <victor@pop-os.localdomain>
2021-06-08 18:06:34 -0500

    first commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"WebSocket/internal/handlers"
+	"github.com/bmizerany/pat"
+	"net/http"
+)
+
+// routes defines the application routes
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
+
+	return mux
+}
